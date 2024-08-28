7ef591d82c0ee9dd5620b81462f53aa9ffd9b77b
PankajKDev <pankajk.dev0000@gmail.com>
2024-08-28 09:11:53 +0530

    finalised app with fixes

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,10 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-  pc: `This is my PC. I do all my programming work on it and I believe I am pretty skilled to check my github btw :D!!
-    Here is my <a href="https://github.com/PankajKDev" target="_blank">Github</a>!`,
-  "drawing": `Haha! that's a drawing I made of me and my gf. I am not that of a great Artist she is tho ;) `,
+  pc: `This is my PC. I do all my programming work on it and I believe I am pretty skilled and familiar with frameworks like React,Express and other web technologies`,
+  "drawing": `Haha! that's a drawing I made of me and my gf. I am not that great of a  Artist but she is tho ;) `,
   "sofa-table": `That's a sofa dude nothing Interesting about it !!!`,
-  tv: `I watch YouTube on It..Sometimes learn new stuff on big screen better than a monitor sometimes`,
+  tv: `I watch YouTube on It..Sometimes learn new stuff on big screen better than a monitor sometimes :D`,
   bed: `This is where I sleep.I get few of my Ideas while I sleep.Once I dreamt of making a Horse tinder and lets say its a work in progress`,
   resume: `This is my resume it took me 2 years almost to make it this good hope it impresses you <a href="https://docs.google.com/document/d/1-1LfKD7FTQgOAtKXzB3HVD444c6eQQXyH1VjR1SbDx4/edit?usp=sharing" target="_blank">Check it out?</a>
   Contact me at pankajk.dev0000@gmail.com if you have any interesting job opportunities!`,
